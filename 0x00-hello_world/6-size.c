67ec511d0ffa9a65fb59b55a8f02b914bd2cef60
IBRAHIMAlla <brahimalla973@gmail.com>
2023-06-15 11:47:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-printf("size of a char: %d byte(s)\n", siezof(char));
-printf("size of an int: %d byte(s)\n", siezof(int));
-printf("size of a long int: %d byte(s)\n", siezof(long int));
-printf("size of a long long int: %d byte(s)\n", siezof(long long int));
-printf("size of a float: %d byte(s)\n", siezof(float));
+printf("size of a char: %zu byte(s)\n", siezof(char));
+printf("size of an int: %zu byte(s)\n", siezof(int));
+printf("size of a long int: %zu byte(s)\n", siezof(long int));
+printf("size of a long long int: %zu byte(s)\n", siezof(long long int));
+printf("size of a float: %zu byte(s)\n", siezof(float));
 return (0);
 }
