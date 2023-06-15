cfe5d49c137203edd290f96b1569d769bbe366dc
IBRAHIMAlla <brahimalla973@gmail.com>
2023-06-15 16:31:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
 * main - Entry point
@@ -7,5 +8,6 @@
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+sizeof("and that piece of art is useful\" - Dora korpar, 2015-10-19");
 return (1);
 }
