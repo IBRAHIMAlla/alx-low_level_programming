41fc295b28e7101cdb1e00a765a805fa270acd24
IBRAHIMAlla <brahimalla973@gmail.com>
2023-06-15 18:33:59 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
+
 /**
-* main - prints "and that piece of art is useful" - Dora korpar, 2015-10-19",
-* followed by a new line, to the standard error.
-* Return: Always 0.
+* main - prints and that piece of art is useful
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
