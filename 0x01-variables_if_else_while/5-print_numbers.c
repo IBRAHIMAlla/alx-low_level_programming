ccd7a38efc60343b945bc816b83bd46e6ccaaf0b
IBRAHIMAlla <brahimalla973@gmail.com>
2023-06-16 13:35:44 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: end program
+*/
+int main(void)
+{
+int j;
+for (j = 0; j < 10; j++)
+printf("%d", j);
+printf("\n");
+return (0);
+}
