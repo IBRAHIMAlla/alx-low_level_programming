4e1da703a9f1015876b5d67be7f67f37ca5ded83
IBRAHIMAlla <brahimalla973@gmail.com>
2023-06-16 13:48:49 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: end program
+*/
+int main(void)
+{
+char B;
+for (B = 'z'; B >= 'a'; B--)
+putchar(B);
+putchar('\n');
+return (0);
+}
