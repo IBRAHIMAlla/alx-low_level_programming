4ae821484255f07b38c041bf3dc570552049bdb5
IBRAHIMAlla <brahimalla973@gmail.com>
2023-06-16 13:03:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabetic
+*
+* Return: end program
+*/
+int main(void)
+{
+char B;
+for (B = 'a'; B <= 'z'; B++)
+putchar(B);
+putchar('\n');
+return (0);
+}
