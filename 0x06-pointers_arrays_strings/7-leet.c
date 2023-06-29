ce052fc33022a2b316497d84c66273d780286b25
IBRAHIMAlla <brahimalla973@gmail.com>
2023-06-29 13:16:19 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int t, y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (t = 0; n[t] != '\0'; t++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (n[t] == s1[t])
+			{
+				n[t] = s2[y];
+			}
+		}
+	}
+	return (n);
+}
