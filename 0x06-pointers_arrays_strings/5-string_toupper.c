1617f034deb02f46680062dadabacc6c0d71c7f9
IBRAHIMAlla <brahimalla973@gmail.com>
2023-06-29 13:00:04 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int o;
+
+	o = 0;
+	while (n[o] != '\0')
+	{
+		if (n[o] >= 'a' && n[o] <= 'z')
+			n[o] = n[o] - 32;
+		o++;
+	}
+	return (n);
+}
