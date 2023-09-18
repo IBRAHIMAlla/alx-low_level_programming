1cb3ceb2475abda7af18db25c23470f9f874c220
IBRAHIMAlla <brahimalla973@gmail.com>
2023-09-18 14:31:48 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/IBRAHIMAlla/alx-low_level_programming/raw/master/0x18-dynamic_libraries/ibraall.so
-export LD_PRELOAD="$PWD/../ibraall.so"
+export LD_PRELOAD=./ibraall.so
+/gm 9 8 10 24 75 9
