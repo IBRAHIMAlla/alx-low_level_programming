251a059a17bf478d4fd94accee53da39b65bc93a
IBRAHIMAlla <brahimalla973@gmail.com>
2023-09-18 08:31:13 -0700

    atoi.c

---
+++ 0x18-dynamic_libraries/atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int m = 0, b = 0, k = 0, le = 0, j = 0, cont = 0;
+
+
+	while (s[le] != '\0')
+		le++;
+
+	while (m < le && f == 0)
+	{
+		if (s[m] == '-')
+			++b;
+
+		if (s[m] >= '0' && s[m] <= '9')
+		{
+			cont = s[m] - '0';
+			if (b % 2)
+				cont = -cont;
+			k = k * 10 + cont;
+			j = 1;
+			if (s[m + 1] < '0' || s[m + 1] > '9')
+				break;
+			j = 0;
+		}
+		m++;
+	}
+
+	if (j == 0)
+		return (0);
+
+	return (k);
+}
