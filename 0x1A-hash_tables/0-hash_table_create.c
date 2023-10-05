0fbfc2e72781f3cfc840f45b9712cfdd23b0574d
IBRAHIMAlla <brahimalla973@gmail.com>
2023-10-05 11:36:09 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,21 +8,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ha_table;
+	hash_table_t *ht;
 
 	if (!size)
 		return (NULL);
 
-	ha_table = calloc(1, sizeof(hash_table_t));
-	if (!ha_table)
+	ht = calloc(1, sizeof(hash_table_t));
+	if (!ht)
 		return (NULL);
-	ha_table->size = size;
-	ha_table->array = calloc(size, sizeof(hash_node_t *));
-	if (!ha_table->array)
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (!ht->array)
 	{
-		free(ha_table);
+		free(ht);
 		return (NULL);
 	}
 
-	return (ha_table);
+	return (ht);
 }
