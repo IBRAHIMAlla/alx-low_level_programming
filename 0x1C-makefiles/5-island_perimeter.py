8761726dcc0bf872494eff77d3c4977d739395b8
IBRAHIMAlla <brahimalla973@gmail.com>
2023-11-21 23:49:59 -0800

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter module
+"""
+
+def island_perimeter(grid):
+    """function returns the perimeter of the island described in grid"""
+    dim = 0
+
+    for m in range(len(grid)):
+        for y in range(len(grid[m])):
+            if grid[m][y]:
+                dim += 4
+
+                if m > 0 and grid[m - 1][y] == 1:
+                    dim -= 2
+
+                if y > 0 and grid[m][y - 1] == 1:
+                    dim -= 2
+
+    return dim
